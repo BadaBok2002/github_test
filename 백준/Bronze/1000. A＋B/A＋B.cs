483a18f2f96f646eaf6a33fbc32320c1a122f744
BadaBok2002 <113076975+BadaBok2002@users.noreply.github.com>
2023-03-29 13:12:37 +0900

    [Bronze V] Title: A+B, Time: 88 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] inputArr = Console.ReadLine().Split();//위에서 받은 값을 나눔. 기준은 공백. 예로
+
+            int a = int.Parse(inputArr[0]);
+            int b = int.Parse(inputArr[1]);
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
